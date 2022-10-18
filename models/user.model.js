cf00f43c0373af0dcd05f828d1690d40eae99a19
Jobayer_ak <jobayer.ak@gmail.com>
2022-10-18 23:59:52 +0600

    user schema is done

--- models/user.model.js
+++ models/user.model.js
@@ -2,58 +2,89 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.userSchema({
-  firstName: {
-    type: String,
-    required: [true, "Please provide a first name"],
-    trim: true,
-    minLength: [3, "Name must be at least 3 characters"],
-    maxLength: [40, "Name is too large"],
-  },
-  lastName: {
-    type: String,
-    required: [true, "Please provide last name"],
-    trim: true,
-    minLength: [3, "Please provide last name"],
-    maxLength: [40, "Name is too large"],
-  },
-  email: {
-    type: String,
-    validate: [validator.isEmail, "Provide a valid Email!"],
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: [true, "Email address is required"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    validate: {
-      validator: (value) =>
-        validator.isStrongPassword(value, {
-          minLength: 6,
-          minLowercase: 3,
-          minNumbers: 1,
-          minUppercase: 1,
-          minSymbol: 1,
-        }),
-      messae: "Password {VALUE} is not strong enough.",
+const userSchema = mongoose.userSchema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please provide a first name"],
+      trim: true,
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [40, "Name is too large"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Please provide last name"],
+      trim: true,
+      minLength: [3, "Please provide last name"],
+      maxLength: [40, "Name is too large"],
+    },
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email!"],
+      trim: true,
+      lowercase: true,
+      unique: true,
+      required: [true, "Email address is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: (value) =>
+          validator.isStrongPassword(value, {
+            minLength: 6,
+            minLowercase: 3,
+            minNumbers: 1,
+            minUppercase: 1,
+            minSymbol: 1,
+          }),
+        messae: "Password {VALUE} is not strong enough.",
+      },
+    },
+    confirmPassword: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: (value) => value === this.password,
+        message: "Password doesn't match",
+      },
+    },
+    contactNumber: {
+      type: String,
+      validate: [
+        validator.isMobilePhone,
+        "Please provide a valid contact number!",
+      ],
+    },
+    shippingAddress: String,
+
+    imageURL: {
+      type: String,
+      validate: [validator.isURL, "Please provide a valid url"],
+    },
+    role: {
+      type: String,
+      enum: ["admin, candidate, hiringmanager"],
+      default: "candidate",
     },
   },
-  confirmPassword: {
-    type: String,
-    required: [true, "Please confirm your password"],
-    validate: {
-      validator: (value) => value === this.password,
-      message: "Password doesn't match",
-    },
-  },
-  role: {
-    type: String,
-    enum: ["admin, candidate, hiringmanager"],
-    default: "candidate",
-  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const password = this.password;
+  const hashedPassword = bcrypt.hashSync(password);
+
+  this.password = hashedPassword;
+  this.confirmPassword = undefined;
+
+  next();
 });
+
+userSchema.methods.comparePassword = function (password, hash) {
+  const isPasswordValid = bcrypt.compareSync(password, hash);
+  return isPasswordValid;
+};
 
 const User = mongoose.model("User", userSchema);
 
