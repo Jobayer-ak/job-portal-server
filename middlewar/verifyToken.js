7db83d40c0fdab177a4759d6573c9d0f111947f5
Jobayer_ak <jobayer.ak@gmail.com>
2022-10-19 23:56:14 +0600

    jwt expire date is updated

--- middlewar/verifyToken.js
+++ middlewar/verifyToken.js
@@ -14,6 +14,7 @@ module.exports = async (req, res, next) => {
         });
     };
 
+
     const decodedData = await promisify(jwt.verify)(token, process.env.TOKEN_SECRET);
     
     req.user = decodedData;
