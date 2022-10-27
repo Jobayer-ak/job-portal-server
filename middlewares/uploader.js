b06a5b50aa856b24453d381b52d140bdb2097677
Jobayer_ak <jobayer.ak@gmail.com>
2022-10-27 23:19:49 +0600

    uploader file is commented

--- middlewares/uploader.js
+++ middlewares/uploader.js
@@ -1,29 +1,29 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: "resumes/",
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e4);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "resumes/",
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e4);
+//     cb(null, uniqueSuffix + "-" + file.originalname);
+//   },
+// });
 
-const uploader = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    const supportedImage = /pdf|docx/;
-    const extension = path.extname(file.originalname);
+// const uploader = multer({
+//   storage: storage,
+//   fileFilter: (req, file, cb) => {
+//     const supportedImage = /pdf|docx/;
+//     const extension = path.extname(file.originalname);
 
-    if (supportedImage.test(extension)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Must be a pdf or docx file!"));
-    }
-  },
-  limits: {
-    fileSize: 5000000,
-  },
-});
+//     if (supportedImage.test(extension)) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error("Must be a pdf or docx file!"));
+//     }
+//   },
+//   limits: {
+//     fileSize: 5000000,
+//   },
+// });
 
-module.exports = uploader;
+// module.exports = uploader;
